4d37e8d3c2fa842e3d453c2b33fff0ffba1f8dc5
SUI <347363545@qq.com>
2017-08-11 16:28:15 +0800

    1628

---
+++ index.php
@@ -0,0 +1,4 @@
+$tempFile = $_FILES['Filedata']['tmp_name'];//获取FILES的缓存文件
+$targetPath = "/site/";//要保存到的新目录
+$targetFile = "1.jpg";//要生成的文件名
+move_uploaded_file($tempFile,$targetPath.$targetFile);//PHP自带函数
