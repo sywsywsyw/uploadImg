dc0b3fd0eab91cc4f0abc1ab41c7d4b52c190a78
SUI <347363545@qq.com>
2017-08-20 02:51:51 +0800

    0251

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+$src = $_FILES['file']['tmp_name'];
+$dist = APP . '/static/imgs/' . $_FILES['file']['name'];
+move_uploaded_file($src, $dist);
+
 $tempFile = $_FILES['Filedata']['tmp_name'];//获取FILES的缓存文件
 $targetPath = "/site/";//要保存到的新目录
 $targetFile = "1.jpg";//要生成的文件名
